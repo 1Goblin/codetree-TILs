5d6ae73fc0162d48fd66c0b064e79a8b573e7734
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-03-20 23:58:31 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240320/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.py
@@ -0,0 +1,12 @@
+n = int(input())
+cnt=0
+
+for i in range(n):
+    for j in range(i):
+        print(" ",end=" ")
+    for k in range(n-i):
+        cnt+=1
+        if cnt==10:
+            cnt=1
+        print(cnt,end=" ")
+    print()
