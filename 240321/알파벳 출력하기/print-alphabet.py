ee0338bdf52790090a127ec96083fe00d5e52172
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-03-21 14:43:44 +0900

    Title: 알파벳 출력하기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240321/알파벳 출력하기/print-alphabet.py
+++ 240321/알파벳 출력하기/print-alphabet.py
@@ -3,8 +3,8 @@ x = 'A'
 
 for i in range(n):
     for j in range(i+1):
-        print(x,end="")
         x = chr(ord(x)+1)
+        print(x,end="")
         if x=='Z':
             x='A'
     print()
