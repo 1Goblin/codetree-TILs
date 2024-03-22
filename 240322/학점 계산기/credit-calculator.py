1257cdeffffdda12f596e2f309648f77fd653827
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-03-22 14:28:10 +0900

    Title: 학점 계산기, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240322/학점 계산기/credit-calculator.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = list(map(float, input().split()))
+
+s = 0
+for i in range(n):
+    s +=arr[i]
+g = s/n
+print(f"{g:.1f}")
+if g>=4.5:
+    print("Perfect")
+elif g>=3.0:
+    print("Good")
+else:
+    print("Poor")
