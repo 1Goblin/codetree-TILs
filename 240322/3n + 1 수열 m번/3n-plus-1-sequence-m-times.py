b4f230d6b7ced956ff791747fc528bae29c08e6d
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-03-22 12:49:34 +0900

    Title: 3n + 1 수열 m번, Time: 113ms, Memory: 25MB, Status: Passed - CodeTree

--- 240322/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
+++ 240322/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
@@ -4,12 +4,13 @@ for _ in range(m):
     cnt = 0
     n = int(input())
     while (1):
+        if n==1:
+            break;
+        
         if n%2==0:
             n=n//2
         else:
             n=n*3+1
         cnt+=1
-        if n==1:
-            break;
 
     print(cnt)
