8a6b903bea57964a32e751ce74cabc08b4679570
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-03-24 14:09:42 +0900

    Title: 짝수들을 거꾸로 출력, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240324/짝수들을 거꾸로 출력/print-even-numbers-upside-down.py
+++ 240324/짝수들을 거꾸로 출력/print-even-numbers-upside-down.py
@@ -1,11 +1,7 @@
 n = int(input())
 
 arr = list(map(int, input().split()))
-a = []
 
-for i in arr:
-    if i%2==0:
-        a.append(i)
-b = a[::-1]
-for k in b:
-    print(k,end=" ")
+for i in range(n-1,-1,-1):
+    if arr[i]%2==0:
+        print(arr[i],end=" ")
