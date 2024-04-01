4a363c2e8b17ae2c53af4d3fa7a4565322111966
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-04-01 16:12:58 +0900

    Title: 나눗셈의 나머지, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240401/나눗셈의 나머지/remainder-of-division.py
+++ 240401/나눗셈의 나머지/remainder-of-division.py
@@ -2,16 +2,13 @@ arr = input().split()
 a = int(arr[0])
 b = int(arr[1])
 
-count =[0]*10
+count = [0] * b
 s = 0
-while (1):
+while a > 1:
+    count[a % b] += 1
+    a = a // b
 
-    count[a%b]+=1
-    a=a//b
-    if a==0:
-        break
-
-for k in range(0,10):
-    s+=count[k]*count[k]
+for k in range(b):
+    s += count[k] * count[k]
 
 print(s)
