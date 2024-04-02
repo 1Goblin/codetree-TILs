0eafe051b3a84f7b4bf1dd67f3f3f68dc0ecf620
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-04-02 15:12:02 +0900

    Title: 연속부분수열일까, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240402/연속부분수열일까/contiguous-array-or-not.py
+++ 240402/연속부분수열일까/contiguous-array-or-not.py
@@ -6,10 +6,11 @@ an = "Yes"
 for i in range(a):
     if n1[i]==n2[0]:
         for j in range(b):
-            if n1[i+j]==n2[j]:
-                an ="Yes"
-            else:
-                an= "No"
+            if (a-i)>b:
+                if n1[i+j]==n2[j]:
+                    an ="Yes"
+                else:
+                    an= "No"
     else:
         an = "No"
 
