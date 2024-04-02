33851bb5c2ca6ca3c95456a08d32596b64f3b79f
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-04-02 14:58:23 +0900

    Title: 연속부분수열일까, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240402/연속부분수열일까/contiguous-array-or-not.py
@@ -0,0 +1,14 @@
+a, b = tuple(map(int, input().split()))
+n1 = list(map(int, input().split()))
+n2 = list(map(int, input().split()))
+c = 0
+an = "Yes"
+for i in n2:
+    if i in n1:
+        c = n1.index(i)
+        for j in range(c,b+1):
+            if n1[j]==n2[j-c]:
+                an="Yes"
+            else:
+                an="No"
+print(an)
