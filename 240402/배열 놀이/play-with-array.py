81603f77ea92402023eda7ba19effab6d5632ddf
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-04-02 14:29:43 +0900

    Title: 배열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240402/배열 놀이/play-with-array.py
@@ -0,0 +1,20 @@
+n, q = tuple(map(int, input().split()))
+values = list(map(int, input().split()))
+
+for i in range(1,n+1):
+    arr = list(map(int, input().split()))
+    a = int(arr[0])
+    b = int(arr[1])
+
+    if a==1:
+        print(values[b-1])
+    elif a==2:
+        if b in values:
+            print((values.index(b)+1))
+        else:
+            print(0)
+    elif a==3:
+        c = int(arr[2])
+        for i in range(b, c+1):
+            print(values[i-1],end=" ")
+        print()
