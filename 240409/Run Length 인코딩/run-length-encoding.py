fa823d2a3e10d9e0e83d3b5e397a370a46e14ad0
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-04-09 15:45:14 +0900

    Title: Run Length 인코딩, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240409/Run Length 인코딩/run-length-encoding.py
+++ 240409/Run Length 인코딩/run-length-encoding.py
@@ -1,22 +1,23 @@
 string = input()
 
-st = ""
 cnt = 0
-for i in range(len(string)-1):
 
-    if string[i]==string[i+1]:
+st = ""
+
+ca = string[0]
+
+for i in range(1, len(string)):
+    if ca==string[i]:
         cnt+=1
-        if i==len(string)-2:
-            st+=string[i]+str(cnt+1)
+        
     else:
-        st+=string[i]+str(cnt+1)
+        st+=ca+str(cnt+1)
+        ca=string[i]
         cnt=0
-        if i==len(string)-2:
-            st+=string[i+1]+"1"
+    
+    if i==len(string)-1:
+        st+=ca+str(cnt+1)
 
-
-if len(string)==1:
-    st=string+"1"
 
 print(len(st))
 print(st)
