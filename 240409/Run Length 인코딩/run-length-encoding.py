2e9b47eb3b2f552fd9799a114317407446002e00
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-04-09 15:06:58 +0900

    Title: Run Length 인코딩, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240409/Run Length 인코딩/run-length-encoding.py
+++ 240409/Run Length 인코딩/run-length-encoding.py
@@ -11,6 +11,9 @@ for i in range(0,len(string)-1):
         cnt=0
     if i==len(string)-2:
         st+=string[i+1]+str(cnt+1)
+if len(string)==1:
+    st = string
+    
 
 print(len(st))
 print(st)
