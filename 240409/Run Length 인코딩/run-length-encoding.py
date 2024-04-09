3d12067965536248846381feff203b1ddea7369f
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-04-09 15:26:12 +0900

    Title: Run Length 인코딩, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240409/Run Length 인코딩/run-length-encoding.py
+++ 240409/Run Length 인코딩/run-length-encoding.py
@@ -5,7 +5,7 @@ cnt = 0
 for i in range(len(string)-1):
     if len(string)==1:
         st=string+"1"
-    
+abcdefg
     if string[i]==string[i+1]:
         cnt+=1
         if i==len(string)-2:
@@ -14,7 +14,7 @@ for i in range(len(string)-1):
         st+=string[i]+str(cnt+1)
         cnt=0
         if i==len(string)-2:
-            st+=string[i]+"1"+string[i+1]+"1"
+            st+=string[i+1]+"1"
 
 print(len(st))
 print(st)
