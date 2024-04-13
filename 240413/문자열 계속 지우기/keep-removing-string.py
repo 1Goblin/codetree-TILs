f5be39ba86e9c410ca24e2416cc073d1369224e4
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-04-13 22:21:11 +0900

    Title: 문자열 계속 지우기, Time: 106ms, Memory: 25MB, Status: Passed - CodeTree

--- 240413/문자열 계속 지우기/keep-removing-string.py
+++ 240413/문자열 계속 지우기/keep-removing-string.py
@@ -3,14 +3,15 @@ b = input()
 
 al = len(a)
 bl = len(b)
-c=0
-while(c<10):
+c=al
+d=0
+while(d<c):
     for i in range(al):
         if a[i:i+bl] == b:
             a = a[0:i] + a[i+bl::]
             al = len(a)
             break;
-    c+=1
+    d+=1
     
 
 print(a)
