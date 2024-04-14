86d980e999c6c25713dd7eace2db8e7d8d6d04e0
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-04-14 17:55:46 +0900

    Title: 소문자와 숫자, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240414/소문자와 숫자/letter-and-number.py
+++ 240414/소문자와 숫자/letter-and-number.py
@@ -3,7 +3,7 @@ n = input()
 for i in n:
     if i>="A" and i<="Z":
         print(i.lower(),end="")
-    elif i>="1" and i<="9":
+    elif i>="0" and i<="9":
         print(i,end="")
     elif i>="a" and i<="z":
         print(i,end="")
