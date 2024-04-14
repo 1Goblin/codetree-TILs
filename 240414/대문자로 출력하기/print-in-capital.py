f4983a7307ef01ce921fd0bb2741e9be45e6f19a
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-04-14 17:46:15 +0900

    Title: 대문자로 출력하기, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240414/대문자로 출력하기/print-in-capital.py
+++ 240414/대문자로 출력하기/print-in-capital.py
@@ -1,11 +1,12 @@
 n = input()
 
 f = ord("A")
+x = ord("Z")
 m = ord("a")
 e = ord("z")
 
 for i in n:
-    if ord(i)>=f and ord(i)<m:
+    if ord(i)>=f and ord(i)<=x:
         print(i,end="")
     elif ord(i)>=m and ord(i)<=e:
         print(chr(ord(i)-m+f),end="")
