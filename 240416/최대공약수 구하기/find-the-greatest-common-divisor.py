af4f70b9a8e399a7da1536e485a7d156e912cc9c
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-04-16 14:50:41 +0900

    Title: 최대공약수 구하기, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240416/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 240416/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -2,12 +2,12 @@ n, m = tuple(map(int, input().split()))
 
 def pr(n, m):
     if n>m:
-        for i in range(m,1,-1):
+        for i in range(m,0,-1):
             if n%i==0 and m%i==0:
                 print(i)
                 break
     elif n<=m:
-        for i in range(m,1,-1):
+        for i in range(m,0,-1):
             if n%i==0 and m%i==0:
                 print(i)
                 break
