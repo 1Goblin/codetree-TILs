63c5316176c24f427603f13562d5f9cdfbf7b82a
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-04-17 14:01:18 +0900

    Title: 함수를 이용한 소수 판별, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240417/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,14 @@
+a, b = tuple(map(int,input().split()))
+
+def padd(num1, num2):
+    addNum = 0
+    for i in range(num1, num2+1):
+        for j in range(2,i):
+            if i%j==0:
+                break
+            if j==i-1:
+                addNum+=i
+    return addNum
+
+
+print(padd(a,b))
