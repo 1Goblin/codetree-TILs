8c6660a5dd7dd5de4a695820d6c04cfa1a14856a
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-04-19 11:46:45 +0900

    Title: 2개 이상의 알파벳, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240419/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 240419/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -2,15 +2,14 @@ a = input()
 
 
 def stcheck(st):
-    cnt = 0
-    for i in range(len(st)-1):
-        for j in range(i+1,len(st)):
-            if st[i]==st[j]:
-                cnt+=1
-        if cnt>=2:
-            return True
-        cnt = 0
-    return False
+    arr = []
+    for i in st:
+        if i not in arr:
+            arr.append(i)
+    if len(arr)>=2:
+        return True
+    else:
+        return False
 
 if stcheck(a):
     print("Yes")
