698aed309e93d1fa013e639eb358371091c8d089
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-04-25 15:14:36 +0900

    Title: 2개씩 그룹짓기, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

--- 240425/2개씩 그룹짓기/group-of-pairs.py
+++ 240425/2개씩 그룹짓기/group-of-pairs.py
@@ -1,10 +1,9 @@
 n = int(input())
 arr = list(map(int, input().split()))
-
+max_num = 0
 arr.sort()
 
 for i in range(n):
-    max_num = 0
     if max_num<arr[i]+arr[(2*n-1)-i]:
         max_num = arr[i]+arr[(2*n-1)-i]
 
