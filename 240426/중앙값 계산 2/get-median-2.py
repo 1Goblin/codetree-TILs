067c2f07a0e54e8ae61a6c0bfa6a4ae738cbf7af
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-04-26 11:09:12 +0900

    Title: 중앙값 계산 2, Time: 109ms, Memory: 25MB, Status: Passed - CodeTree

--- 240426/중앙값 계산 2/get-median-2.py
+++ 240426/중앙값 계산 2/get-median-2.py
@@ -2,11 +2,7 @@ n = int(input())
 arr = list(map(int, input().split()))
 na = []
 
-def mid(ar,n):
-    return ar[n//2]
-
 for i in range(n):
-    na.append(arr[i])
     if i%2==0:
-        na.sort()
+        na = sorted(arr[:i+1])
         print(na[i//2],end=" ")
