9c42c7dc4302dbe89a0a0f124de0a3f14c1494bd
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-04-28 12:55:34 +0900

    Title: 비오는 날, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240428/비오는 날/rainy-day.py
+++ 240428/비오는 날/rainy-day.py
@@ -9,12 +9,13 @@ class weather:
 
 arr = []
 rain_day = 0
+md = ":"
 for i in range(n):
     date, dw, wt = input().split()
     arr.append(weather(date, dw, wt))
-    if wt == "Rain":
+    if wt == "Rain" and md>date:
         rain_day = i
-        break
+        md = date
     
 
 print(f"{arr[rain_day].date} {arr[rain_day].dw} {arr[rain_day].wt}")
