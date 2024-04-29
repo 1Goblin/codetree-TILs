7e2ea87089d91f9a66a6cb97524b5ba33d9ffe14
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-04-29 17:17:28 +0900

    Title: 체크판위에서 2, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240429/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,23 @@
+r, c = map(int, input().split())
+arr = [
+    list(map(str, input().split()))
+    for _ in range(r)
+]
+
+cnt = 0
+p_word = arr[0][0]
+
+
+for i in range(1, r-2):
+    for j in range(1, c-2):
+        p_word = arr[0][0]
+        if p_word != arr[i][j]:
+            p_word = arr[i][j]
+            for m in range(i+1, r-1):
+                for n in range(j+1, c-1):
+                    p_word = arr[i][j]
+                    if p_word != arr[m][n]:
+                        p_word = arr[m][n]
+                        cnt+=1
+
+print(cnt)
