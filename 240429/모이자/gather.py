1e42ca968fc7e4442e8dba7bc15f9e5ec0135c02
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-04-29 15:11:08 +0900

    Title: 모이자, Time: 152ms, Memory: 72MB, Status: Passed - CodeTree

--- 240429/모이자/gather.py
+++ 240429/모이자/gather.py
@@ -5,13 +5,13 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 min_dis = sys.maxsize
-sumh = 0
+
 for i in range(n):
+    sumh = 0
     for j in range(n):
         sumh += arr[j] * abs(i-j)
     if min_dis > sumh:
         min_dis = sumh
-    sumh = 0
 
 
 
