49d144beeea9c4935310d4abe646b334a4ace329
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-04-30 21:56:14 +0900

    Title: 작은 구슬의 이동, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240430/작은 구슬의 이동/small-marble-movement.py
+++ 240430/작은 구슬의 이동/small-marble-movement.py
@@ -13,13 +13,14 @@ d = da[d]
 r = int(r) -1
 c = int(c) -1
 
+
 def ran(x, y):
     return x>=0 and y>=0 and x<n and y<n
 
 
+dx = [-1, 0, 0, 1]
+dy = [0, 1, -1, 0]
 
-dx = [0, 1, -1, 0]
-dy = [-1, 0, 0, 1]
 
 arr = [
     [0 for _ in range(n)]
@@ -39,4 +40,5 @@ for i in range(t):
         d = 3-d
 
 
-print(c, r)
+
+print(r+1, c+1)
