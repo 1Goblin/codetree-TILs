0ca033724a00512032f89e37de7d4c09321df196
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-04-30 21:44:07 +0900

    Title: 작은 구슬의 이동, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240430/작은 구슬의 이동/small-marble-movement.py
+++ 240430/작은 구슬의 이동/small-marble-movement.py
@@ -19,7 +19,7 @@ def ran(x, y):
 
 
 dx = [0, 1, -1, 0]
-dy = [1, 0, 0, -1]
+dy = [-1, 0, 0, 1]
 
 arr = [
     [0 for _ in range(n)]
@@ -29,16 +29,14 @@ arr = [
 
 for i in range(t):
 
-    if not ran(r,c):
-        d = 3-d
-    else: 
-        arr[r][c] = 0
-        r= r+dx[d]
-        c= c+dy[d]
-        arr[r][c] = 1
+    nx = r + dx[d]
+    ny = c + dy[d]
 
-for i in range(n):
-    for j in range(n):
-        if arr[i][j] == 1:
-            print(j, i)
-            break
+    if ran(nx,ny):
+        r += dx[d]
+        c += dy[d]
+    else:
+        d = 3-d
+
+
+print(c, r)
