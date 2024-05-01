9e36bc50b3856dc24a6741df252e26f23ee0cefb
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-05-01 13:42:08 +0900

    Title: 이상한 진수 2, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240501/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,16 @@
+n = list(map(int, input()))
+
+result = 0
+for i in range(len(n)):
+    if n[i]==0:
+        n[i] = 1
+        break
+
+l = len(n) -1
+for j in range(len(n)):
+    result+=n[j]*2**l
+    l-=1
+
+
+
+print(result)
