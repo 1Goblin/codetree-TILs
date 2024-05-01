4a5847a4b66e6cfc7415cccebdbea57becf4698d
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-05-01 18:48:31 +0900

    Title: 최고의 33위치, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

--- 240501/최고의 33위치/best-place-of-33.py
+++ 240501/최고의 33위치/best-place-of-33.py
@@ -6,13 +6,13 @@ arr = [
 ]
 
 max_cnt = -sys.maxsize
+
 for i in range(n-2):
     for j in range(n-2):
         cnt = 0
         for m in range(i,i+3):
-            for n in range(j, j+3):
-                if arr[m][n] == 1:
+            for k in range(j, j+3):
+                if arr[m][k] == 1:
                     cnt+=1
-        
         max_cnt = max(max_cnt, cnt)
 print(max_cnt)
