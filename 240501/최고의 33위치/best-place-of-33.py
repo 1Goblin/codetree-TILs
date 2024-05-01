18b05e098c31c72b180d6d7ae0a08c958cf89423
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-05-01 18:37:43 +0900

    Title: 최고의 33위치, Time: 196ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240501/최고의 33위치/best-place-of-33.py
+++ 240501/최고의 33위치/best-place-of-33.py
@@ -10,7 +10,7 @@ for i in range(n-2):
     for j in range(n-2):
         cnt = 0
         for m in range(i,i+3):
-            for n in range(i, i+3):
+            for n in range(j, j+3):
                 if arr[m][n] == 1:
                     cnt+=1
         
