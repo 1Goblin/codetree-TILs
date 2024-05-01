d8623673fd5742263ed3cf81d2517620315b65a8
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-05-01 14:31:03 +0900

    Title: G or H 3, Time: 1288ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240501/G or H 3/G-or-H-3.py
@@ -0,0 +1,29 @@
+n, k = tuple(map(int, input().split()))
+
+d ={
+    "G" : 1,
+    "H" : 2,
+    0 : 0
+}
+
+arr = []
+max_n = 0
+for i in range(n):
+    p, a = input().split()
+    p = int(p)
+    arr.append([p,a])
+    max_n = max(max_n, p)
+
+narr = [0] * (max_n + 1)
+
+for i in arr:
+    narr[i[0]] = i[1]
+
+max_s = 0
+for i in range(1, max_n-k+1):
+    s = 0
+    for j in range(i, k+2):
+        s+=d[narr[j]] 
+    max_s = max(max_s, s)
+
+print(max_s)
