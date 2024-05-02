9f1611348ad9a79651e08921ce9103aa7dac1162
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-05-02 13:01:12 +0900

    Title: 숫자들의 합 중 최대, Time: 160ms, Memory: 25MB, Status: Passed - CodeTree

--- 240502/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
+++ 240502/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
@@ -3,16 +3,17 @@ import sys
 x, y = tuple(map(int, input().split()))
 
 
+def sn(num):
+
+    if num<10:
+        return num
+
+    return sn(num//10) + num%10
+
 max_c = -sys.maxsize
 
 for i in range(x, y+1):
-    cnt = 0
-    while (1):
-        if i<=0:
-            break
-        cnt+=i%10
-        i = i//10
-    
-    max_c = max(max_c, cnt)
+
+    max_c = max(sn(i),max_c)
 
 print(max_c)
