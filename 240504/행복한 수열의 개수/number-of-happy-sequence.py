6144dbeb58759e883ddde29e7e46df982e7057ae
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-05-04 12:30:22 +0900

    Title: 행복한 수열의 개수, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240504/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,32 @@
+def check(arr, m):
+    c = 0
+
+    for i in range(len(arr)-m+1):
+        for j in range(i+1,i+m):
+            if arr[i]==arr[j]:
+                c+=1
+    if c>=(m-1):
+        return True
+    else:
+        return False
+
+n, m = tuple(map(int, input().split()))
+cnt = 0
+
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+for i in range(n):
+
+    carr = [cal[i] for cal in arr]
+
+    if check(arr[i], m):
+        cnt+=1
+
+    if check(carr, m):
+        cnt+=1
+
+
+print(cnt)
