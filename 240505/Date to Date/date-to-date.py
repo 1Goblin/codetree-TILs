e72b7c1f3936e127e93d58be3fce4215d1583ac2
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-05-05 12:45:12 +0900

    Title: Date to Date, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240505/Date to Date/date-to-date.py
+++ 240505/Date to Date/date-to-date.py
@@ -2,7 +2,7 @@ m1, d1, m2, d2 = list(map(int, input().split()))
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 if m1 == m2:
-    ans = d2 - d1
+    ans = d2 - d1 + 1
 else:
     ans = num_of_days[m1] - d1 + 1 + d2  # d1 일 포함
     for i in range(m1 + 1, m2):
