36067a77157e755b5ea9015343247373a3e6b8c2
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-05-11 15:35:58 +0900

    Title: 벌금은 누구에게, Time: 182ms, Memory: 25MB, Status: Passed - CodeTree

--- 240511/벌금은 누구에게/who-will-pay.py
+++ 240511/벌금은 누구에게/who-will-pay.py
@@ -2,17 +2,15 @@ n, m, k = tuple(map(int, input().split()))
 
 arr = [0]*(n+1)
 
-for _ in range(m):
-    num = int(input())
-
-    arr[num] +=1
-
 ans = -1
 
-for i,num in enumerate(arr):
-    if num>=k:
-        ans = i
-        print(ans)
+for _ in range(m):
+    num = int(input())
+    arr[num] +=1
+    if arr[num]>=k:
+        print(num)
+        ans = 1
+        break
 
 if ans==-1:
-    print(ans)
+    print(-1)
