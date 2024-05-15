1831071b4af612718b9db85cbd8e2bd8c718e8cb
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-05-15 13:13:51 +0900

    Title: 두 점으로 만드는 직사각형, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240515/두 점으로 만드는 직사각형/rectangle-made-of-two-points.py
+++ 240515/두 점으로 만드는 직사각형/rectangle-made-of-two-points.py
@@ -2,17 +2,7 @@ a1, b1, a2, b2 = tuple(map(int, input().split()))
 x1, y1, x2, y2 = tuple(map(int, input().split()))
 
 
-def cal(x1,x2,y1,y2):
-    return (x2-x1)*(y2-y1)
+width = max(x2,a2) - min(a1,x1)
+height = max(b2,y2) - min(b1,y1)
 
-
-if x1>a1:
-    if y1>b1:
-        print(cal(a1,x2,b1,y2))
-    else:
-        print(cal(a1,x2,y1,b2))
-else:
-    if y1>b1:
-        print(cal(x1,a2,b1,y2))
-    else:
-        print(cal(x1,a2,y1,b2))
+print(width*height)
