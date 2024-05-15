6ca5912beb224590840d82618903d538f5b6e29b
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-05-15 13:43:33 +0900

    Title: 행복한 수열의 개수, Time: 131ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240515/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240515/행복한 수열의 개수/number-of-happy-sequence.py
@@ -10,18 +10,26 @@ ans = 0
 
 for i in range(n):
     r = 0
+    max_r = 0
     for j in range(n):
         if j!=0 and arr[i][j] == arr[i][j-1]:
             r+=1
+        else:
+            max_r = max(max_r,r)
+            r = 0
     
     if (r+1)>=m:
         ans+=1
 
 for i in range(n):
     c = 0
+    max_c = 0
     for j in range(n):
         if j!=0 and arr[j][i] == arr[j-1][i]:
             c+=1
+        else:
+            max_c=max(max_c,c)
+            c = 0
     if (c+1)>=m:
         ans+=1
 
