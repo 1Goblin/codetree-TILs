9929aa5a941f99a275c16fb65e7640802ece7060
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-05-16 20:59:06 +0900

    Title: 그래프 탐색, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240516/그래프 탐색/graph-traversal.py
@@ -0,0 +1,23 @@
+n, m = tuple(map(int, input().split()))
+
+arr =[ [] for _ in range(n+1)]
+visited = [ False for _ in range(n+1)]
+
+cnt = 0
+
+for _ in range(m):
+    x, y = tuple(map(int, input().split()))
+    arr[x].append(y)
+
+
+def dfs(vertex):
+    global cnt
+    for curr_v in arr[vertex]:
+        if not visited[curr_v]:
+            cnt+=1
+            visited[curr_v] = True
+            dfs(curr_v)
+
+dfs(1)
+
+print(cnt)
