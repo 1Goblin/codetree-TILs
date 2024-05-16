eb97012b2ac3732a2998f36b71c919068025c18c
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-05-16 15:19:05 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 290ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 240516/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
@@ -0,0 +1,18 @@
+k, n = tuple(map(int, input().split()))
+
+ans = []
+
+
+def choose(num):
+
+    if num > n:
+        print(*ans, sep=" ")
+        return
+    
+    for i in range(1,k+1):
+        ans.append(i)
+        choose(num+1)
+        ans.pop()
+
+
+choose(1)
