a456f38975d37f070731d5a950f7b8344249c6f3
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-05-17 14:56:26 +0900

    Title: 최소 경로로 탈출 하기, Time: 184ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240517/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+n, m = tuple(map(int, input().split()))
+
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+visited = [
+    [0 for _ in range(m)]
+    for _ in range(n)
+]
+
+seq = [
+    [0 for _ in range(m)]
+    for _ in range(n)
+]
+
+q = deque()
+
+def ok_go(r, c):
+    return r>=0 and c>=0 and r<n and c<m and visited[r][c] == 0 and arr[r][c] == 1
+
+
+def bfs():
+    dxs, dys = [1,-1,0,0], [0,0,1,-1]
+    while q:
+        r, c = q.popleft()
+        for dx, dy in zip(dxs,dys):
+            ndx, ndy = r+dx, c+dy
+            if ok_go(ndx, ndy):
+                seq[ndx][ndy] = seq[r][c]+1
+                visited[ndx][ndy] = 1
+                q.append((ndx,ndy))
+
+
+q.append((0,0))
+visited[0][0] = 1
+bfs()
+
+# for i in range(n):
+#     for j in range(m):
+#         print(seq[i][j],end=" ")
+#     print()
+
+print(seq[-1][-1] if seq[-1][-1] != 0 else -1)
