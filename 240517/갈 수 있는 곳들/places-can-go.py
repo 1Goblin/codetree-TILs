ee449b54cd5da37ced926f8670b9044c1f801287
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-05-17 16:59:00 +0900

    Title: 갈 수 있는 곳들, Time: 270ms, Memory: 27MB, Status: Passed - CodeTree

--- 240517/갈 수 있는 곳들/places-can-go.py
+++ 240517/갈 수 있는 곳들/places-can-go.py
@@ -2,7 +2,6 @@ from collections import deque
 
 n, k = tuple(map(int, input().split()))
 
-
 arr = [
     list(map(int, input().split()))
     for _ in range(n)
@@ -13,30 +12,21 @@ visited = [
     for _ in range(n)
 ]
 
-cnt = 0
-
-
 dxs, dys = [1,-1,0,0], [0,0,1,-1]
 
 def can_go(r,c):
     return r>=0 and c>=0 and r<n and c<n and visited[r][c] == 0 and arr[r][c] == 0
 
-
 def bfs():
-    global cnt
     while q:
         r, c = q.popleft()
         for dx, dy in zip(dxs,dys):
             ndx, ndy = r+dx, c+dy
             if can_go(ndx,ndy):
-                cnt+=1
                 visited[ndx][ndy] = 1
                 q.append((ndx,ndy))
 
-
-
 for _ in range(k):
-
 
     q = deque()
     r,c = tuple(map(int, input().split()))
@@ -44,10 +34,12 @@ for _ in range(k):
     c -=1
     if can_go(r,c):
         visited[r][c] = 1
-        cnt+=1
         q.append((r,c))
         bfs()
+ans = 0
 
+for i in range(n):
+    for j in range(n):
+        ans+=visited[i][j]
 
-
-print(cnt)
+print(ans)
