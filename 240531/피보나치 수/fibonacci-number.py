5ed81a80d68dba5b8edec69217be78870b305496
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-05-31 21:43:12 +0900

    Title: 피보나치 수, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240531/피보나치 수/fibonacci-number.py
+++ 240531/피보나치 수/fibonacci-number.py
@@ -1,9 +1,26 @@
+# n = int(input())
+
+# dp = [1] * (n+1)
+
+
+# for i in range(3,n+1):
+#     dp[i] = dp[i-1] + dp[i-2]
+
+# print(dp[n])
+
 n = int(input())
 
-dp = [1] * (n+1)
+mem = [-1] * (n+1)
 
+def f(num):
+    if mem[num] != -1:
+        return mem[num]
 
-for i in range(3,n+1):
-    dp[i] = dp[i-1] + dp[i-2]
+    if num<=2:
+        return 1
+    
+    mem[num] = mem[num-1] + mem[num-2]
 
-print(dp[n])
+    return f(num-1) + f(num-2)
+
+print(f(n))
