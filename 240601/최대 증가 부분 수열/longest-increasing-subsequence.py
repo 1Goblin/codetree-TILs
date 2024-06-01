004fc9327a2ae6cc0b4662b3895e7462e14f14ea
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-06-01 16:25:53 +0900

    Title: 최대 증가 부분 수열, Time: 158ms, Memory: 25MB, Status: Passed - CodeTree

--- 240601/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 240601/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -3,10 +3,10 @@ arr = list(map(int, input().split()))
 
 ans = []
 
-dp = [0] * (n+1)
+dp = [1] * (n+1)
 dp[0] = 1
 
-for i in range(1,n):
+for i in range(n):
     for j in range(i):
         if arr[j] >= arr[i]:
             continue
