ccaf27c256427c702ec6e4a22ee78005d9beb6c7
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-06-01 18:24:12 +0900

    Title: 동전 거슬러주기, Time: 382ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240601/동전 거슬러주기/coin-change.py
+++ 240601/동전 거슬러주기/coin-change.py
@@ -3,20 +3,23 @@ arr = list(map(int, input().split()))
 
 dp = [0] * m
 
+t = 0
 
 for i in arr:
     if i>m:
-        print(-1)
+        t = -1
     else:
         dp[i-1] = 1
 
 for i in range(m):
     for j in range(i):
-        if (i-j) in arr and dp[j] != 0:
+        if (i-j) in arr and dp[j] != 0 and t !=-1:
             if dp[i] == 0:
                 dp[i] = dp[j] + 1
             else:
                 dp[i] = min(dp[i], dp[j] + 1)
 
-
-print(dp[m-1])
+if t==-1:
+    print(-1)
+else:
+    print(dp[m-1])
