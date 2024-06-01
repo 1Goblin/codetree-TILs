212fdd82f72b2e92dd7887e85255a9d3d840bfae
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-06-01 17:26:43 +0900

    Title: 동전 거슬러주기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240601/동전 거슬러주기/coin-change.py
+++ 240601/동전 거슬러주기/coin-change.py
@@ -3,17 +3,17 @@ import sys
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-dp = [sys.maxsize] * (m)
+dp = [10001] * (m)
 
 for i in arr:
     dp[i-1] = 1
 
 for i in range(m):  #동전 금액 
     for j in range(i):  #비교할 동전 금액 
-        if (i-j) in arr and dp[j] != sys.maxsize:
+        if (i-j) in arr and dp[j] != 10001:
             dp[i] = min(dp[i], dp[j]+1)
 
-if dp[m-1] == sys.maxsize:
+if dp[m-1] == 10001:
     print(-1)
 else:
     print(dp[m-1])
