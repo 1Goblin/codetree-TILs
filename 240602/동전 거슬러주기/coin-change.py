31fe0333ffd8c8842ea4bc2b2dfb1a2317607f3b
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-06-02 15:41:03 +0900

    Title: 동전 거슬러주기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240602/동전 거슬러주기/coin-change.py
@@ -0,0 +1,23 @@
+import sys
+
+n, m = tuple(map(int, input().split()))
+
+coin = list(map(int, input().split()))
+
+maxV = sys.maxsize
+
+dp = [maxV] * (m+1)
+
+for i in coin:
+    dp[i] = 1
+
+
+for i in range(1,m+1):
+    for j in range(n):
+        if i>coin[j]:
+            dp[i] = min(dp[i], dp[i-coin[j]] + 1)
+
+if dp[m] == maxV:
+    print(-1)
+else:
+    print(dp[m])
