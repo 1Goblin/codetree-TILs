dbf52897e3bc4dea396e90851e83accd5817356f
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-06-05 20:27:54 +0900

    Title: 최장 공통 부분 수열, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240605/최장 공통 부분 수열/longest-common-sequence.py
@@ -0,0 +1,37 @@
+a = input()
+b = input()
+a = list(a)
+b = list(b)
+a_len = (len(a))
+b_len = (len(b))
+
+dp = [
+    [0 for _ in range(b_len)]
+    for _ in range(a_len)
+]
+
+def initialize():
+    if a[0] == b[0]:
+        dp[0][0] = 1
+
+    for i in range(a_len):
+        if a[i] == b[0]:
+            for j in range(i,a_len):
+                dp[j][0] = 1
+            break
+    
+    for i in range(b_len):
+        if b[i] == a[0]:
+            for j in range(i,b_len):
+                dp[0][j] = 1
+            break
+initialize()
+
+for i in range(1, a_len):
+    for j in range(1, b_len):
+        if a[i] == b[j]:
+            dp[i][j] = dp[i-1][j-1] +1
+        elif a[i-1] == b[j] or a[i] == b[j-1]:
+            dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+
+print(dp[a_len-1][b_len-1])
