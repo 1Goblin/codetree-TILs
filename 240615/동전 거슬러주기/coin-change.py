3b43be333d2fd7acb5b2bcdcdadedebdbdff117d
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-06-15 03:01:21 +0900

    Title: 동전 거슬러주기, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240615/동전 거슬러주기/coin-change.py
+++ 240615/동전 거슬러주기/coin-change.py
@@ -1,7 +1,30 @@
+# import sys
+
+# n, m = tuple(map(int, input().split()))
+
+# coin = list(map(int, input().split()))
+
+# maxV = sys.maxsize
+
+# dp = [maxV] * (m+1)
+
+# dp[0] = 0
+
+
+# for i in range(1,m+1):
+#     for j in range(n):
+#         if i>=coin[j] and dp[i-coin[j]] != maxV:
+#             dp[i] = min(dp[i], dp[i-coin[j]] + 1)
+
+# if dp[m] == maxV:
+#     print(-1)
+# else:
+#     print(dp[m])
+
+
 import sys
 
 n, m = tuple(map(int, input().split()))
-
 coin = list(map(int, input().split()))
 
 maxV = sys.maxsize
@@ -10,39 +33,13 @@ dp = [maxV] * (m+1)
 
 dp[0] = 0
 
-
 for i in range(1,m+1):
     for j in range(n):
         if i>=coin[j] and dp[i-coin[j]] != maxV:
             dp[i] = min(dp[i], dp[i-coin[j]] + 1)
 
+
 if dp[m] == maxV:
     print(-1)
 else:
     print(dp[m])
-
-
-# import sys
-
-# n, m = tuple(map(int, input().split()))
-# arr = list(map(int, input().split()))
-
-# maxV = sys.maxsize
-
-# dp = [maxV] * (m+1)
-
-# for i in arr:
-#     dp[i] = 1
-
-# dp[0] = 0
-
-# for i in range(1,m+1):
-#     for coin in arr:
-#         if i>=coin and dp[i-coin] != maxV:
-#             dp[i] = min(dp[i-coin] + 1, dp[i])
-
-
-# if dp[m] == maxV:
-#     print(-1)
-# else:
-#     print(dp[m])
