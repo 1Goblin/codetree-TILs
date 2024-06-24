dc26d16afdc05bbd398eca920871ea894514c408
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-06-25 01:12:45 +0900

    Title: 특정 위치의 문자, Time: 177ms, Memory: 14MB, Status: Passed - CodeTree

--- 240625/특정 위치의 문자/char-in-specific-location.js
+++ 240625/특정 위치의 문자/char-in-specific-location.js
@@ -4,9 +4,18 @@ let n = fs.readFileSync(0).toString().trim()
 
 let arr = ['L', 'E', 'B', 'R', 'O', 'S']
 
+let ans = -1
+
 for(let i=0; i<arr.length; i++){
     if(arr[i] === n){
-        console.log(i)
+        ans = i
         break
     }
+}
+
+if(ans === -1){
+    console.log("None")
+}
+else{
+    console.log(ans)
 }
