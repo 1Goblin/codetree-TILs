b7afb1a7ea10622490f3415ff96c19f1074a2f4d
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-06-30 21:55:53 +0900

    Title: Date to Date, Time: 184ms, Memory: 47MB, Status: Passed - CodeTree

--- 240630/Date to Date/date-to-date.js
+++ 240630/Date to Date/date-to-date.js
@@ -5,11 +5,16 @@ let [m1,d1,m2,d2] = input.split(" ").map(Number)
 
 const num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-let a = num_of_days[m1] - d1
-let b = 0
+let c = 0
 
-for(let i=m1+1; i<m2; i++){
-    b += num_of_days[i]
+if(m1 !== m2){
+    for(let i =m1+1; i<m2; i++){
+        c+=num_of_days[i] 
+    }
+    c += num_of_days[m1] - d1 + d2
+}else{
+    c = d2 - d1
 }
 
-console.log(a+b+d2+1)
+
+console.log(c+1)
