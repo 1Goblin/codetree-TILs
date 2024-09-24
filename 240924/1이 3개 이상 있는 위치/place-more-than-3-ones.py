e898e4c1610fc1b599b82746f2dd8e32fa739445
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-09-24 18:26:42 +0900

    Title: 1이 3개 이상 있는 위치, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240924/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -0,0 +1,27 @@
+n = int(input())
+arr = [
+    list(map(int, input().split(' ')))
+    for _ in range(n)
+]
+
+ans = 0
+
+ndx, ndy = [0,0,1,-1], [1,-1,0,0]
+
+def inRange(x,y):
+    if (x<0 or y<0 or x>=n or y>=n):
+        return True
+    return False
+
+for x in range(n):
+    for y in range(n):
+        count = 0
+        for dx, dy in zip(ndx, ndy):
+            if((inRange(x+dx, y+dy))):
+                continue
+
+            if(arr[x+dx][y+dy] == 1):
+                count+=1
+        if(count>=3):
+            ans +=1
+print(ans)
