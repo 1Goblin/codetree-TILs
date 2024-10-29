2c8894d243dc3ba3a77da8dcbd9a402f67f36136
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-10-29 11:38:15 +0900

    Title: 2줄 출력, Time: 80ms, Memory: 8MB, Status: Passed - CodeTree

--- 241029/2줄 출력/print-two-lines.java
+++ 241029/2줄 출력/print-two-lines.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.prinltn("Hello");
+        System.out.println("Hello");
         System.out.println("World");
         // 여기에 코드를 작성해주세요.
     }
