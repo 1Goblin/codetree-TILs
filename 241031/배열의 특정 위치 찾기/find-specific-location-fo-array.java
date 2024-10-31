d1bb67fcca1ef847ebd998b9303d4dccc90fa499
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-10-31 15:00:44 +0900

    Title: 배열의 특정 위치 찾기, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241031/배열의 특정 위치 찾기/find-specific-location-fo-array.java
+++ 241031/배열의 특정 위치 찾기/find-specific-location-fo-array.java
@@ -4,17 +4,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int sum = 0;
+        int sum2 = 0;
+        int cnt = 0;
         int[] arr = new int[10];
 
         for(int i=0; i<10; i++){
             arr[i] = sc.nextInt();
         }
 
-        for(int i=1; i<10; i+=2){
-            sum+=arr[i];
+        for(int i=1; i<10; i++){
+            if(i%2==1){
+                sum+=arr[i];
+            }else if((i+1)%3==0){
+                sum2+=arr[i];
+                cnt++;
+            }
         }
 
-        double avg = (double)sum/5;
+
+        double avg = (double)sum2/cnt;
 
         System.out.printf("%d %.1f",sum, avg);
         // 여기에 코드를 작성해주세요.
