69840d896ebbc9a99cd72ad5e652b61d7c3964b5
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-11-01 13:49:12 +0900

    Title: 문자열 두번 출력하기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241101/문자열 두번 출력하기/print-string-twice.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+
+        System.out.println(a);
+        System.out.println(a);
+        // 여기에 코드를 작성해주세요.
+    }
+}
