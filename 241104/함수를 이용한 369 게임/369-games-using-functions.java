59303152f9de71d540d954ac1ef9c6953b0821ef
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-11-04 16:19:31 +0900

    Title: 함수를 이용한 369 게임, Time: 156ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241104/함수를 이용한 369 게임/369-games-using-functions.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean has369(int n){
+        int ans = n;
+
+        while(true){
+            
+            if((ans%10) %3 ==0){
+                return true;
+            }
+
+            if(ans/10 == 0){
+                return false;
+            }
+            ans = ans/10;
+
+        }
+
+    }
+
+    public static boolean mul3(int n){
+        return n%3==0;
+    }
+
+    public static int answerNum(int a,int b){
+        int ans = 0;
+        for(int i=a; i<=b; i++){
+            if(has369(i) || mul3(i)){
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(answerNum(a,b));
+        // 여기에 코드를 작성해주세요.
+    }
+}
