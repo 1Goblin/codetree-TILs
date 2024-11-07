b9f259db5bbae7f62682f25a8ae0a5ef818f1a11
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-11-07 14:52:57 +0900

    Title: Top k 숫자 구하기, Time: 173ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241107/Top k 숫자 구하기/kth-number.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void sortArr(int[] arr){
+        Arrays.sort(arr);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        sortArr(arr);
+
+        System.out.println(arr[k-1]);
+        // 여기에 코드를 작성해주세요.
+    }
+}
