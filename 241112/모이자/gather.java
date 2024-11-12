e24174dbc2256184d38c66fb73977eff818acc45
1Goblin <133861328+1Goblin@users.noreply.github.com>
2024-11-12 16:14:08 +0900

    Title: 모이자, Time: 160ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241112/모이자/gather.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int minAbs = 100000;
+
+        for(int i=0; i<n; i++){
+            int ans = 0;
+            for(int j=0; j<n; j++){
+                ans += arr[j] * Math.abs(j-i);
+            }
+            if(minAbs > ans){
+                minAbs = ans;
+            }
+        }
+
+        System.out.println(minAbs);
+        
+
+        // 여기에 코드를 작성해주세요.
+    }
+}
